fc019c6ff8db62c5c016d0eeb5195ce5b6730c99
Ehtesham Zahid <ehteshamzahid313@gmail.com>
2026-07-21 02:50:38 +0500

    Github Auth Integration

--- apps/server/src/types/session.d.ts
+++ apps/server/src/types/session.d.ts
@@ -2,7 +2,9 @@ import 'express-session';
 
 declare module 'express-session' {
   interface SessionData {
-    githubAccessToken?: string;
     userId?: string;
+    githubAccessToken?: string;
   }
 }
+
+export {};
